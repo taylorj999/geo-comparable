86209c3c778391b1a4991a1376c7b8b8d19846a2
taylorj999 <taylorj999@gmail.com>
2019-08-16 13:20:08 -0400

    Added the babel precompiler to package.json and the jest config.

---
+++ setup.jest.js
@@ -0,0 +1,11 @@
+// setupTests.js
+
+import 'regenerator-runtime/runtime'
+/*
+import Enzyme from 'enzyme'
+import EnzymeAdapter from 'enzyme-adapter-react-16'
+
+Enzyme.configure({
+  adapter: new EnzymeAdapter()
+})
+*/
