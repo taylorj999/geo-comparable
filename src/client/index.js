be10e60d7d51a6af31a26d18818dc78915742f56
taylorj999 <taylorj999@gmail.com>
2019-08-21 15:44:53 -0400

    Added axios library to the requirements.

--- src/client/index.js
+++ src/client/index.js
@@ -2,5 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'axios/dist/axios.min.js';
 
 ReactDOM.render(<App />, document.getElementById('root'))
