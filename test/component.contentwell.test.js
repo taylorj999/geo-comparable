5a413b00290bc942c8e3737b453ebe2f31e98a45
taylorj999 <taylorj999@gmail.com>
2019-08-27 19:27:20 -0400

    Updated content well test.

--- test/component.contentwell.test.js
+++ test/component.contentwell.test.js
@@ -3,9 +3,13 @@ import React from 'react'
 import {mount} from 'enzyme'
 
 describe('ContentWell', () => {
-	test('test that content well state buttons work', () => {
-		const wrapper = mount(<ContentWell/>);
-		
+	var wrapper = null;
+	
+	beforeAll(() => { wrapper = mount(<ContentWell/>); });
+	
+	beforeEach(() => { wrapper.setState({contentPage: "default"}); });
+	
+	test('test that default search result button works', () => {
 		const button = wrapper.find("#defaultSearchResultsButton");
 		expect(button.length).toBe(1);
 		
@@ -14,5 +18,16 @@ describe('ContentWell', () => {
 		expect(wrapper.state('contentPage')).toEqual("defaultSearchResults");
 		
 	    expect(wrapper).toMatchSnapshot();
-	    })
+	    });
+	
+	test('test that property search button works', () => {
+		const button = wrapper.find("#propertySearchButton");
+		expect(button.length).toBe(1);
+		
+		button.simulate('click');
+		
+		expect(wrapper.state('contentPage')).toEqual("propertySearch");
+		
+	    expect(wrapper).toMatchSnapshot();
+	});
 })
