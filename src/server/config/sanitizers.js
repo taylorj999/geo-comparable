9593615364fb32357bb524a1d5508aa7b82cdad1
taylorj999 <taylorj999@gmail.com>
2019-09-10 13:34:25 -0400

    Added numeric only input sanitizer

--- src/server/config/sanitizers.js
+++ src/server/config/sanitizers.js
@@ -2,5 +2,6 @@ var sanitizers = {};
 
 //standard is alphanumeric plus space
 sanitizers.standard = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890 ';
+sanitizers.numeric = '1234567890';
 
 module.exports = sanitizers;
