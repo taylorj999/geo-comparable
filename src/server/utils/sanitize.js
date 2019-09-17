986aaf05c181d262c9c6779adb67368d7f1d1f75
taylorj999 <taylorj999@gmail.com>
2019-09-17 18:24:28 -0400

    Added a check to safely pass numeric typed input

--- src/server/utils/sanitize.js
+++ src/server/utils/sanitize.js
@@ -1,12 +1,16 @@
 var sanitizers = require('../config/sanitizers');
 var validator = require('validator');
+var inputchecker = require('./inputchecker').inputchecker;
 
 function sanitize () {
 	"use strict";
 }
 
 sanitize.prototype.cleanInput = function cleanInput(uncleanInput, sanitizer) {
-	if (sanitizer === undefined) {
+	var checker = new inputchecker();
+	if (checker.isValidNumericInput(uncleanInput)) {
+		return uncleanInput;
+	} else if (sanitizer === undefined) {
 		return validator.whitelist(uncleanInput,sanitizers.standard);
 	} else {
 		return validator.whitelist(uncleanInput,sanitizer);
