b6243370695179b8bb1c6c7ff0120da58a5e4ba7
taylorj999 <taylorj999@gmail.com>
2019-09-19 12:41:16 -0400

    Added file-loader for images

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,17 @@ const moduleObj = {
 		{
 			test: /\.css$/,
 			use: ['style-loader','css-loader']
-		},]
+		},
+		{
+		    test: /\.(svg|png|jpg|jpeg|gif)$/,
+		    use: {
+		        loader: 'file-loader',
+		        options: {
+		            name: '[name].[ext]',
+		            outputPath: 'images'
+		        }
+		    }
+		}]
 };
 
 const client = {
