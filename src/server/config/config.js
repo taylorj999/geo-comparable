a7ca074cdb8912504bad7fe488d947c757d47dcf
taylorj999 <taylorj999@gmail.com>
2019-09-19 10:43:42 -0400

    Additional system config values

--- src/server/config/config.js
+++ src/server/config/config.js
@@ -8,9 +8,16 @@ config.system.database = {};
 
 config.system.database.host = "localhost";
 config.system.database.dbname = "test";
-config.system.database.user = "nottherealuser";
-config.system.database.password = "nottherealpassword";
+config.system.database.user = "__DBUSERHERE__";
+config.system.database.password = "__DBPWDHERE__";
+config.system.database.port = 3306;
 
 config.system.serverPort 		= 3011;
+
+// how many possibilities, maximum, to return on an autocompletion query
+config.system.autoCompleteLimit = 10;
+
+// how many properties, maximum, to return on a property search query
+config.system.propSearchLimit = 10;
 
 module.exports = config;
