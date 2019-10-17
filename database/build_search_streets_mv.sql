0b1177633b55e4fdadb9cf6071a653af9a831508
taylorj999 <taylorj999@gmail.com>
2019-10-17 07:16:49 -0400

    Triggers and creation procedure for the search streets view.

---
+++ database/build_search_streets_mv.sql
@@ -0,0 +1,13 @@
+DROP PROCEDURE IF EXISTS BuildSearchStreets;
+
+DELIMITER $$
+CREATE PROCEDURE BuildSearchStreets()
+BEGIN
+  INSERT INTO search_Streets (ogr_fid, streetname, shape)
+  SELECT ogr_fid, full_stree, st_geomfromtext(st_astext(st.shape))
+    FROM streets st
+  ON DUPLICATE KEY UPDATE shape = st_geomfromtext(st_astext(st.shape));
+END$$
+DELIMITER ;
+
+CALL BuildSearchStreets();
--- database/triggers.sql
+++ database/triggers.sql
@@ -27,3 +27,37 @@ BEGIN
   DELETE FROM search_parcels WHERE ogr_fid = OLD.ogr_fid;
 END;$$
 DELIMITER ;
+
+DROP TRIGGER IF EXISTS insert_street_trigger;
+
+delimiter $$
+CREATE TRIGGER insert_street_trigger AFTER INSERT ON streets
+FOR EACH ROW
+BEGIN
+  INSERT INTO search_streets (ogr_fid, streetname, shape)
+  VALUES (NEW.ogr_fid, CONCAT(NEW.street_nam, ' ', NEW.street_typ), st_geomfromtext(st_astext(NEW.shape)));
+END;$$
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS update_street_trigger;
+
+delimiter $$
+CREATE TRIGGER update_street_trigger AFTER UPDATE ON streets
+FOR EACH ROW
+BEGIN
+  UPDATE search_streets SET streetname = CONCAT(NEW.street_nam, ' ', NEW.street_typ), 
+                            shape = st_geomfromtext(st_astext(NEW.shape))
+						WHERE ogr_fid = NEW.ogr_fid;
+END;$$
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS delete_street_trigger;
+
+delimiter $$
+CREATE TRIGGER delete_street_trigger AFTER DELETE ON streets
+FOR EACH ROW
+BEGIN
+  DELETE FROM search_streets WHERE ogr_fid = OLD.ogr_fid;
+END;$$
+DELIMITER ;
+
