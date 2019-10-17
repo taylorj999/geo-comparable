b5d374ccb7714af01228fd57f269cff214c0c17c
taylorj999 <taylorj999@gmail.com>
2019-10-17 18:53:54 -0400

    Correct case error.

--- database/build_search_streets_mv.sql
+++ database/build_search_streets_mv.sql
@@ -3,7 +3,7 @@ DROP PROCEDURE IF EXISTS BuildSearchStreets;
 DELIMITER $$
 CREATE PROCEDURE BuildSearchStreets()
 BEGIN
-  INSERT INTO search_Streets (ogr_fid, streetname, shape)
+  INSERT INTO search_streets (ogr_fid, streetname, shape)
   SELECT ogr_fid, full_stree, st_geomfromtext(st_astext(st.shape))
     FROM streets st
   ON DUPLICATE KEY UPDATE shape = st_geomfromtext(st_astext(st.shape));
