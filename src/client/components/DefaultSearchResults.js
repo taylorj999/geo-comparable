27f3163385f7b3dd9734b456787b994425950e27
taylorj999 <taylorj999@gmail.com>
2019-10-23 12:41:14 -0400

    Update example properties.

--- src/client/components/DefaultSearchResults.js
+++ src/client/components/DefaultSearchResults.js
@@ -1,24 +1,35 @@
 import React, { Component } from 'react';
 import PropertyDetailList from './PropertyDetailList';
-import MapnikImage from './MapnikImage';
+import MapnikImageContainer from './MapnikImageContainer';
 
 var defaultPropertyList = require('../config/defaultPropertyList');
 
 export default class MainMenu extends Component {
   constructor(props) {
 	super(props);
-	this.state = { properties : defaultPropertyList };
+	this.state = { properties : defaultPropertyList, currentProperty : defaultPropertyList[0].ogr_fid };
   }
-    
+  
+  updateMap(propertyId) {
+	  this.setState({currentProperty:propertyId});
+  }
+
   render() {
     return (
       <div className="container" id="defaultSearchResults">
-        <div className="row">
-          <div className="col">
-            <PropertyDetailList properties={this.state.properties}/>
+	    <div className="row mt-1">
+          <div className="col-6">
+            <PropertyDetailList properties={this.state.properties} mapFunction={this.updateMap.bind(this)} currentProperty={this.state.currentProperty}/>
           </div>
-          <div className="col-4">
-            <MapnikImage propertyId={this.state.properties[0].ogr_fid} key={this.state.properties[0].ogr_fid}/>
+  	      <div className="col-6">
+          {function(){
+            if( this.state.currentProperty ) {
+              let imgKey = this.state.currentProperty;
+              return <MapnikImageContainer propertyId={this.state.currentProperty} key={imgKey}/>
+            } else {
+              return null;	
+            }
+          }.bind(this)()}
           </div>
         </div>
       </div>
--- src/client/config/defaultPropertyList.js
+++ src/client/config/defaultPropertyList.js
@@ -1,3 +1,10 @@
-let properties = [{ogr_fid:704, address: "456 Test Ave", price:100000},{ogr_fid:2, address: "123 Test St", price: 50000}];
+let properties = [{ogr_fid:60198, address: "83 MOUNTAIN VIEW RD", price: 199900},
+	              {ogr_fid:118595, address: "321 BEAVERDAM RD", price: 300800},
+	              {ogr_fid:114000, address: "10 DEANWOOD CIR", price: 192800},
+	              {ogr_fid:96112, address: "6 DAISY LN", price: 289500},
+	              {ogr_fid:18548, address: "275 ASBURY RD", price: 166200},
+	              {ogr_fid:37834, address: "41 STANDHILL DR", price: 149000},
+	              {ogr_fid:102949, address: "43 STAMEY RD", price: 326500},
+	              {ogr_fid:103227, address: "129 RIDGEDALE RD", price: 120200}];
 
 module.exports = properties;
